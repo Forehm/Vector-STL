750d0f8c0f445aecca16a2d561fa1c5e302f9ef2
Egor <112934317+Forehm@users.noreply.github.com>
2023-09-06 16:42:11 +0300

    Update Vector.h

--- Vector.h
+++ Vector.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <iostream>
-#include <vector>   
-
+#include <vector>
 
 
 class VectorException : public std::exception
@@ -75,7 +74,7 @@ public:
         bool operator!=(const iterator& rhs) { return ptr_ != rhs.ptr_; }
 
 
-private:
+    private:
         T* ptr_;
     };
 
@@ -100,7 +99,7 @@ private:
     class reverse_iterator
     {
     public:
-    
+
         reverse_iterator(T* ptr) : ptr_(ptr) { }
         reverse_iterator operator++() { reverse_iterator i = *this; ptr_--; return i; }
         reverse_iterator operator++(int junk) { ptr_--; return *this; }
@@ -116,7 +115,7 @@ private:
     class const_reverse_iterator
     {
     public:
-    
+
         const_reverse_iterator(T* ptr) : ptr_(ptr) { }
         const_reverse_iterator operator++() { const_reverse_iterator i = *this; ptr_--; return i; }
         const_reverse_iterator operator++(int junk) { ptr_--; return *this; }
@@ -147,181 +146,31 @@ private:
 
     const_iterator cend() const { return const_iterator(vector_ + size_); }
 
-    size_t get_capacity() const { return capacity_; }
+    size_t get_capacity() const;
 
-    size_t get_size() const { return size_; }
+    size_t get_size() const;
 
-    bool is_empty() const { return size_ == 0; }
+    bool is_empty() const;
 
-    void reserve(const size_t value)
-    {
-        capacity_ = value;
+    void reserve(const size_t value);
 
-        T* arr = new T[size_ + value];
-        for (size_t i = 0; i < size_; ++i)
-        {
-            arr[i] = vector_[i];
-        }
+    void push_back(const T& value);
 
-        SwapArrays(arr);
+    void shrink_to_fit();
 
-    }
+    void pop_back();
 
-    void push_back(const T& value)
-    {
-        T* arr;
-        if (capacity_ <= 0)
-        {
-            arr = new T[size_ + 1];
-            for (size_t i = 0; i < size_; ++i)
-            {
-                arr[i] = vector_[i];
-            }
-            arr[size_] = value;
-            SwapArrays(arr);
-            ++size_;
+    void insert(const size_t& index, const T& value);
 
-        }
-        else
-        {
-            vector_[size_] = value;
-            if (capacity_ > 0)
-            {
-                --capacity_;
-            }
-            ++size_;
-        }
+    void clear();
 
+    void assign(const size_t& quantity, const T& value);
 
+    void resize(const size_t& new_size);
 
+    T at(const size_t& index);
 
-    }
-    
-    void shrink_to_fit()
-    {
-        capacity_ = 0;
-
-        T* arr = new T[size_];
-        for (size_t i = 0; i < size_; ++i)
-        {
-            arr[i] = vector_[i];
-        }
-
-        SwapArrays(arr);
-
-    }
-
-    void pop_back()
-    {
-        T* arr = new T[size_ - 1];
-
-        for (size_t i = 0; i < size_ - 1; ++i)
-        {
-            arr[i] = vector_[i];
-        }
-
-
-        SwapArrays(arr);
-        --size_;
-    }
-
-    void insert(const size_t& index, const T& value)
-    {
-        T* arr;
-        if (capacity_ <= 0)
-        {
-            arr = new T[size_ + 1];
-            for (size_t i = 0, j = 0; j < size_; ++i, ++j)
-            {
-                if (i == index)
-                {
-                    arr[i] = value;
-                    ++i;
-
-                }
-                arr[i] = vector_[j];
-            }
-
-            SwapArrays(arr);
-
-
-        }
-        else
-        {
-            for (size_t i = size_; i > 0; i--)
-            {
-                if (i == index)
-                {
-                    vector_[i] = value;
-                    break;
-                }
-                else
-                {
-                    vector_[i] = vector_[i - 1];
-                }
-            }
-
-        }
-
-        ++size_;
-
-    }
-
-    void clear()
-    {
-        size_ = 0;
-        capacity_ = 0;
-        T* arr = new T[size_ + capacity_];
-        SwapArrays(arr);
-    }
-
-    void assign(const size_t& quantity, const T& value)
-    {
-        T* arr;
-        arr = new T[quantity];
-        size_ = quantity;
-        capacity_ = 0;
-
-        for (size_t i = 0; i < quantity; i++)
-        {
-            arr[i] = value;
-        }
-
-
-        SwapArrays(arr);
-    }
-
-    void resize(const size_t& new_size)
-    {
-        if (new_size <= 0)
-        {
-            throw VectorException(2);
-        }
-
-        T* arr = new T[new_size];
-
-        for (size_t i = 0; i < new_size; ++i)
-        {
-            arr[i] = vector_[i];
-        }
-
-        SwapArrays(arr);
-
-        size_ = new_size;
-    }
-
-    T at(const size_t& index)
-    {
-        if (index < 0 || index > size_ - 1)
-        {
-            throw VectorException(1);
-        }
-        else
-        {
-            return vector_[index];
-        }
-
-    }  T& front() { return vector_[0]; }
+    T& front();
 
     T& back() { return vector_[size_ - 1]; }
 
@@ -329,199 +178,416 @@ private:
 
     T* rdata() { return this->vector_ + (size_ - 1); }
 
-    Vector(const Vector& v)
-    {
-        this->size_ = v.size_;
-        this->capacity_ = v.capacity_;
+    Vector(const Vector& v);
 
-        vector_ = new T[size_ + capacity_];
+    Vector(const std::vector<T>& v);
 
-        for (size_t i = 0; i < size_; i++)
-        {
-            vector_[i] = v.vector_[i];
-        }
+    Vector(const size_t& new_size);
 
-    }
-
-    Vector(const vector<T>& v)
-    {
-        this->size_ = v.size();
-        this->capacity_ = v.capacity();
-
-        vector_ = new T[size_ + capacity_];
-
-        for (size_t i = 0; i < size_; i++)
-        {
-            vector_[i] = v[i];
-        }
-    }
-
-    Vector(const size_t& new_size)
-    {
-        vector_ = new T[new_size];
-        size_ = new_size;
-    }
-    
-    
-    Vector(const size_t& new_size, const T& value)
-    {
-        if (new_size <= 0)
-        {
-            throw VectorException(2);
-        }
-        vector_ = new T[new_size];
-        size_ = new_size;
-
-        for (size_t i = 0; i < size_; ++i)
-        {
-            vector_[i] = value;
-        }
-    }
+    Vector(const size_t& new_size, const T& value);
 
     Vector() { vector_ = new T[size_]; }
 
-    ~Vector()
-    {
-        delete[]vector_;
-        vector_ = nullptr;
-    }
+    ~Vector();
 
-    size_t count(const T& value)
-    {
-        size_t counter = 0;
+    size_t count(const T& value);
 
-        for (size_t i = 0; i < size_; ++i)
-        {
-            if (vector_[i] == value)
-            {
-                ++counter;
-            }
-        }
+    T& operator = (const Vector& v);
 
-        if (counter < 0)
-        {
-            counter = 0;
-        }
+    T& operator = (const std::vector<T>& v);
 
-        return counter;
-    }
+    T& operator [](const int& index);
 
-    T& operator = (const Vector& v)
-    {
-        this->size_ = v.get_size();
-        T* arr = new T[v.get_size()];
+    bool operator == (const Vector& another);
 
-        if (size_ > 0 && v.get_size() > 0)
-        {
-            for (size_t i = 0; i < size_; ++i)
-            {
-                arr[i] = v.vector_[i];
-            }
+    bool operator != (const Vector& another);
 
-            SwapArrays(arr);
-        }
-        else
-        {
-            throw VectorException(3);
-        }
-
-    }
-   
-    T& operator = (const vector<T>& v)
-    {
-        this->size_ = v.size();
-        T* arr = new T[v.size()];
-
-        if (size_ > 0 && v.size() > 0)
-        {
-            for (size_t i = 0; i < size_; ++i)
-            {
-                arr[i] = v[i];
-            }
-
-            SwapArrays(arr);
-        }
-        else
-        {
-            throw VectorException(3);
-        }
-
-    }
-
-    T& operator [](const int& index)
-    {
-        if (index < 0 || index > size_ - 1)
-        {
-            throw VectorException(1);
-        }
-
-        return vector_[index];
-    }
-
-    bool operator == (const Vector& another)
-    {
-        if (this->get_size() != another.get_size())
-        {
-            return false;
-        }
-
-        for (size_t i = 0; i < this->get_size(); ++i)
-        {
-            if (this->vector_[i] != another.vector_[i])
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    bool operator != (const Vector& another)
-    {
-        if (this->get_size() != another.get_size())
-        {
-            return true;
-        }
-
-        for (size_t i = 0; i < this->get_size(); ++i)
-        {
-            if (this->vector_[i] != another.vector_[i])
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    Vector operator + (const Vector& another)
-    {
-        if (this->get_size() > another.get_size() || this->get_size() == another.get_size())
-        {
-            for (size_t i = 0; i < another.get_size(); ++i)
-            {
-                this->vector_[i] += another.vector_[i];
-            }
-        }
-        else
-        {
-            this->reserve(another.get_size() - this->get_size());
-            size_t i = 0;
-            
-            for (; i < this->get_size(); ++i)
-            {
-                this->vector_[i] += another.vector_[i];
-            }
-
-            for (; i < another.get_size(); ++i)
-            {
-                this->vector_[i] = another.vector_[i];
-            }
-            
-            size_ += capacity_;
-            capacity_ = 0;
-        }
-        return *this;
-    }
+    Vector operator + (const Vector& another);
 
 };
 
 
+template<typename T>
+size_t Vector<T>::get_capacity() const { return capacity_; }
+
+template<typename T>
+size_t Vector<T>::get_size() const { return size_; }
+
+template<typename T>
+bool Vector<T>::is_empty() const { return size_ == 0; }
+
+template<typename T>
+void Vector<T>::reserve(const size_t value)
+{
+    capacity_ = value;
+
+    T* arr = new T[size_ + value];
+    for (size_t i = 0; i < size_; ++i)
+    {
+        arr[i] = vector_[i];
+    }
+
+    SwapArrays(arr);
+}
+
+template<typename T>
+void Vector<T>::push_back(const T& value)
+{
+    T* arr;
+    if (capacity_ <= 0)
+    {
+        arr = new T[size_ + 1];
+        for (size_t i = 0; i < size_; ++i)
+        {
+            arr[i] = vector_[i];
+        }
+        arr[size_] = value;
+        SwapArrays(arr);
+        ++size_;
+
+    }
+    else
+    {
+        vector_[size_] = value;
+        if (capacity_ > 0)
+        {
+            --capacity_;
+        }
+        ++size_;
+    }
+}
+
+template<typename T>
+void Vector<T>::shrink_to_fit()
+{
+    capacity_ = 0;
+
+    T* arr = new T[size_];
+    for (size_t i = 0; i < size_; ++i)
+    {
+        arr[i] = vector_[i];
+    }
+
+    SwapArrays(arr);
+}
+
+template<typename T>
+void Vector<T>::pop_back()
+{
+    T* arr = new T[size_ - 1];
+
+    for (size_t i = 0; i < size_ - 1; ++i)
+    {
+        arr[i] = vector_[i];
+    }
+
+
+    SwapArrays(arr);
+    --size_;
+}
+
+template<typename T>
+void Vector<T>::insert(const size_t& index, const T& value)
+{
+    T* arr;
+    if (capacity_ <= 0)
+    {
+        arr = new T[size_ + 1];
+        for (size_t i = 0, j = 0; j < size_; ++i, ++j)
+        {
+            if (i == index)
+            {
+                arr[i] = value;
+                ++i;
+
+            }
+            arr[i] = vector_[j];
+        }
+
+        SwapArrays(arr);
+
+
+    }
+    else
+    {
+        for (size_t i = size_; i > 0; i--)
+        {
+            if (i == index)
+            {
+                vector_[i] = value;
+                break;
+            }
+            else
+            {
+                vector_[i] = vector_[i - 1];
+            }
+        }
+
+    }
+    ++size_;
+}
+
+template<typename T>
+void Vector<T>::clear()
+{
+    size_ = 0;
+    capacity_ = 0;
+    T* arr = new T[size_ + capacity_];
+    SwapArrays(arr);
+}
+
+template<typename T>
+void Vector<T>::assign(const size_t& quantity, const T& value)
+{
+    T* arr;
+    arr = new T[quantity];
+    size_ = quantity;
+    capacity_ = 0;
+
+    for (size_t i = 0; i < quantity; i++)
+    {
+        arr[i] = value;
+    }
+
+
+    SwapArrays(arr);
+}
+
+template<typename T>
+void Vector<T>::resize(const size_t& new_size)
+{
+    if (new_size <= 0)
+    {
+        throw VectorException(2);
+    }
+
+    T* arr = new T[new_size];
+
+    for (size_t i = 0; i < new_size; ++i)
+    {
+        arr[i] = vector_[i];
+    }
+
+    SwapArrays(arr);
+
+    size_ = new_size;
+}
+
+template<typename T>
+T Vector<T>::at(const size_t& index)
+{
+    if (index < 0 || index > size_ - 1)
+    {
+        throw VectorException(1);
+    }
+    else
+    {
+        return vector_[index];
+    }
+
+}
+
+template<typename T>
+T& Vector<T>::front()
+{
+    return vector_[0];
+}
+
+template<typename T>
+Vector<T>::Vector(const Vector& v)
+{
+    this->size_ = v.size_;
+    this->capacity_ = v.capacity_;
+
+    vector_ = new T[size_ + capacity_];
+
+    for (size_t i = 0; i < size_; i++)
+    {
+        vector_[i] = v.vector_[i];
+    }
+}
+
+template<typename T>
+Vector<T>::Vector(const std::vector<T>& v)
+{
+    this->size_ = v.size();
+    this->capacity_ = v.capacity();
+
+    vector_ = new T[size_ + capacity_];
+
+    for (size_t i = 0; i < size_; i++)
+    {
+        vector_[i] = v[i];
+    }
+}
+
+template<typename T>
+Vector<T>::Vector(const size_t& new_size)
+{
+    vector_ = new T[new_size];
+    size_ = new_size;
+}
+
+template<typename T>
+Vector<T>::Vector(const size_t& new_size, const T& value)
+{
+    if (new_size <= 0)
+    {
+        throw VectorException(2);
+    }
+    vector_ = new T[new_size];
+    size_ = new_size;
+
+    for (size_t i = 0; i < size_; ++i)
+    {
+        vector_[i] = value;
+    }
+}
+
+template<typename T>
+Vector<T>::~Vector()
+{
+    delete[]vector_;
+    vector_ = nullptr;
+}
+
+template<typename T>
+size_t Vector<T>::count(const T& value)
+{
+    size_t counter = 0;
+
+    for (size_t i = 0; i < size_; ++i)
+    {
+        if (vector_[i] == value)
+        {
+            ++counter;
+        }
+    }
+
+    if (counter < 0)
+    {
+        counter = 0;
+    }
+
+    return counter;
+}
+
+template<typename T>
+T& Vector<T>::operator=(const Vector& v)
+{
+    this->size_ = v.get_size();
+    T* arr = new T[v.get_size()];
+
+    if (size_ > 0 && v.get_size() > 0)
+    {
+        for (size_t i = 0; i < size_; ++i)
+        {
+            arr[i] = v.vector_[i];
+        }
+
+        SwapArrays(arr);
+    }
+    else
+    {
+        throw VectorException(3);
+    }
+}
+
+template<typename T>
+T& Vector<T>::operator=(const std::vector<T>& v)
+{
+    this->size_ = v.size();
+    T* arr = new T[v.size()];
+
+    if (size_ > 0 && v.size() > 0)
+    {
+        for (size_t i = 0; i < size_; ++i)
+        {
+            arr[i] = v[i];
+        }
+
+        SwapArrays(arr);
+    }
+    else
+    {
+        throw VectorException(3);
+    }
+}
+
+template<typename T>
+T& Vector<T>::operator[](const int& index)
+{
+    if (index < 0 || index > size_ - 1)
+    {
+        throw VectorException(1);
+    }
+
+    return vector_[index];
+}
+
+template<typename T>
+bool Vector<T>::operator==(const Vector& another)
+{
+    if (this->get_size() != another.get_size())
+    {
+        return false;
+    }
+
+    for (size_t i = 0; i < this->get_size(); ++i)
+    {
+        if (this->vector_[i] != another.vector_[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+template<typename T>
+bool Vector<T>::operator!=(const Vector& another)
+{
+    if (this->get_size() != another.get_size())
+    {
+        return true;
+    }
+
+    for (size_t i = 0; i < this->get_size(); ++i)
+    {
+        if (this->vector_[i] != another.vector_[i])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+template<typename T>
+Vector<T> Vector<T>::operator+(const Vector& another)
+{
+    if (this->get_size() > another.get_size() || this->get_size() == another.get_size())
+    {
+        for (size_t i = 0; i < another.get_size(); ++i)
+        {
+            this->vector_[i] += another.vector_[i];
+        }
+    }
+    else
+    {
+        this->reserve(another.get_size() - this->get_size());
+        size_t i = 0;
+
+        for (; i < this->get_size(); ++i)
+        {
+            this->vector_[i] += another.vector_[i];
+        }
+
+        for (; i < another.get_size(); ++i)
+        {
+            this->vector_[i] = another.vector_[i];
+        }
+
+        size_ += capacity_;
+        capacity_ = 0;
+    }
+    return *this;
+}
